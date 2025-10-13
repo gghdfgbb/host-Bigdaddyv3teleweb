0fcc24411ee6f13e521642ce6a37a794e93a9d35
botphistar001 <affiliatecommunity1@gmail.com>
2025-10-14 00:21:41 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  BOT_TOKEN: "8494420933:AAFNh20zM_RHbP4ftWGcBEusD1VmcQlvg3E",
+  ADMIN_CHAT_ID: "8099343828",
+  WEB_DOMAIN: process.env.WEB_DOMAIN || "https://your-app.onrender.com",
+  PORT: process.env.PORT || 3000
+};
