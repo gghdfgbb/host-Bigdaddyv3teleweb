eb820baf04a0a679540e3f7ee667e144d6c6c647
botphistar001 <affiliatecommunity1@gmail.com>
2025-10-14 03:15:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN || "8494420933:AAFNh20zM_RHbP4ftWGcBEusD1VmcQlvg3E",
   ADMIN_CHAT_ID: process.env.ADMIN_CHAT_ID || "8099343828",
-  WEB_DOMAIN: process.env.WEB_DOMAIN || "http://localhost:3000",
+  WEB_DOMAIN: process.env.WEB_DOMAIN || "https://your-app-name.onrender.com",
   PORT: process.env.PORT || 3000,
-  NODE_ENV: process.env.NODE_ENV || 'development'
+  NODE_ENV: process.env.NODE_ENV || 'production'
 };
